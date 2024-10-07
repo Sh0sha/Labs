e8dd095e14bd4129507229173f20efb835cfea4d
Shohzod Shaimov <100679345+Sh0sha@users.noreply.github.com>
2024-10-07 14:52:49 +0500

    DOne

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewController;
 
 // Определение маршрутов для получения информации о сервере, клиенте и бд
-Route::get('/info/server', [InfoController::class, 'serverInfo']);
-Route::get('/info/client', [InfoController::class, 'clientInfo']);
-Route::get('/info/database', [InfoController::class, 'dbInfo']);
+Route::get('/info/server', [NewController::class, 'serverInfo']);
+Route::get('/info/client', [NewController::class, 'clientInfo']);
+Route::get('/info/database', [NewController::class, 'databaseInfo']);
